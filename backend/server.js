02da296de58fc76945ca7e99f0dd33999b79003d
Harman <harmanwork124@gmail.com>
2024-01-24 10:47:41 -0500

    Ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import { configDotenv } from "dotenv";
 import cookieParser from "cookie-parser";
 
@@ -18,6 +19,18 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 app.use(notFound);
 app.use(errorHandler);
 
--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
     <div className="w-full md:w-6/12 max-w-[35rem] border-slate-600 border-2 rounded-lg my-[5rem] mx-auto px-10 py-20 flex flex-col justify-between">
       <h1 className="text-center font-bold text-4xl">Welcome to Auth</h1>
       <p className="text-center mt-6 mb-4">
-        Login here or register if you don't have an account!
+        You can Register here, or login if you already have an account!
       </p>
       <div className="mx-auto mt-5 flex justify-center gap-8 ">
         <Link
